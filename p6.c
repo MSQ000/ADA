891abebc17a1c774fea48d7789efeff2e16842b6
Mohammed Shamshuddin Qureshi <shamshuddinmsq@gmail.com>
2025-05-12 01:42:48 +0530

    New files added

---
+++ p6.c
@@ -0,0 +1,56 @@
+#include<stdio.h>
+//nclude<conio.h>
+int max(int a,int b)
+{
+    if(a>b) return a;
+    else return b;
+}
+
+void main()
+{
+    int i,j,n,capacity;
+    int w,wt[20],ve[20],v[20][20];
+    printf("Enter the number of items:\n");
+    scanf("%d",&n);
+    printf("\n-----\nWeight values\n-----\n");
+    for(i=1;i<=n;i++)
+    {
+        scanf("%d",&wt[i]);
+        scanf("%d",&ve[i]);
+    }
+    printf("Enter the Capacity of Knapsack :\n");
+    scanf("%d",&capacity);
+    for(i=0;i<=n;i++)
+    {
+        for(j=0;j<=capacity;j++)
+        {
+            if(i==0 || j==0)
+            {
+                v[i][j]=0;
+            }
+            else if(j-wt[i]>=0)
+            {
+                v[i][j]=max(v[i-1][j],v[i-1][j-wt[i]]+ve[i]);
+            }
+            else
+            {
+                v[i][j]=v[i-1][j];
+            }
+            printf("%4d",v[i][j]);
+        }
+        printf("\n");
+    }
+    w=capacity;
+    printf("The items in the Knapsack are :\n");
+    for(i=n;i>0;i--)
+    {
+        if(v[i][w]==v[i-1][w]) continue;
+        else
+        {
+            w=w-wt[i];
+            printf("%3d",wt[i]);
+        }
+    }
+    printf("\nTotal profit: %d",v[n][capacity]);
+    getchar();
+}
---
+++ p8.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+//#include<conio.h>
+#define max 10
+int s[max],x[max],d;
+
+void sumofsub(int p,int k,int r)
+{
+    int i;
+    x[k]=1;
+
+    if(p+s[k]==d)
+    {
+        for(i=1;i<=k;i++)
+        if(x[i]==1)
+         printf("%d\t",s[i]);
+        printf("\n");
+    }
+    else if(p+s[k]+s[k+1]<=d)
+        sumofsub(p+s[k],k+1,r-s[k]);
+    if((p+r-s[k]>=d)&&(p+s[k+1]<=d))
+    {
+        x[k]=0;
+        sumofsub(p,k+1,r-s[k]);
+    }
+}
+
+void main()
+{
+    int i,n,sum=0;
+    printf("Enter the max no:\n");
+    scanf("%d",&n);
+    printf("Enter the subset in increasing order:\n");
+    for(i=1;i<=n;i++)
+     scanf("%d",&s[i]);
+    printf("Enter the max subset:\t");
+    scanf("%d",&d);
+    for(i=1;i<=n;i++)
+        sum=sum+s[i];
+    if(sum<d||s[1]>d)
+        printf("No possible subset\n");
+    else
+        sumofsub(0,1,sum);
+   // getch();
+}
